e0c0fea66ef603e3d312a6b41ea0779fd64e1c9e
Agana-Joseph <josephagana8@gmail.com>
2025-01-30 02:08:11 -0800

    Top Header

--- src/components/layout/navigation/Header.jsx
+++ src/components/layout/navigation/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import { ButtonSolid } from '../../common/base/buttons/Button'
 
 export default function Header() {
   return (
-    <div>
-      <ButtonSolid text='Nav header'></ButtonSolid>
+    <div className='bg-[#000000] flex px-32 text-[#fff] max-h-16'>
+      <div className="txt w-24 flex-1 mt-2">
+        <h1 className='p-2'><span className='bg-[#ece047] p-2'>Black</span> Friday</h1>
+      </div>
+      <div className="offSales w-24 flex flex-1 mt-2 p-2">
+        <p>UPTO</p>
+        <h1 className='text-[#ece047] text-2xl'>59%</h1>
+        <p>OFF</p>
+      </div>
+      <div className="button w-24 ml-24 mt-2">
+        <button className='bg-[#ece047] p-2'>Shope Now</button>
+      </div>
     </div>
   )
 }
