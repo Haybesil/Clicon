69eceb552e4a74588cf255c9b42d455b1dd0cf40
Agana-Joseph <josephagana8@gmail.com>
2025-01-29 19:55:39 -0800

    Changed header text

---
+++ src/components/layout/navigation/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ButtonSolid } from '../../common/base/buttons/Button'
+
+export default function Header() {
+  return (
+    <div>
+      <ButtonSolid text='Nav header'></ButtonSolid>
+    </div>
+  )
+}
